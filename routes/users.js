ad335636d2a5474ed56e10ee5fcf588fd8d41cba
Hassan Siddique <76737340+hassan012345@users.noreply.github.com>
2024-02-15 19:26:00 +0500

    Update users.js with completed code
    Complete lab and added missing and required code.

--- routes/users.js
+++ routes/users.js
@@ -1,59 +1,83 @@
 const express = require('express');
+
 const router = express.Router();
 
+let friends = {
+    "johnsmith@gamil.com": { "firstName": "John", "lastName": "Doe", "DOB": "22-12-1990" },
+    "annasmith@gamil.com": { "firstName": "Anna", "lastName": "smith", "DOB": "02-07-1983" },
+    "peterjones@gamil.com": { "firstName": "Peter", "lastName": "Jones", "DOB": "21-03-1989" }
+};
 
-let users = [
-    {
-        firstName: "John",
-        lastName: "wick",
-        email:"johnwick@gamil.com",
-        DOB:"22-01-1990",
-    },
-    {
-        firstName: "John",
-        lastName: "smith",
-        email:"johnsmith@gamil.com",
-        DOB:"21-07-1983",
-    },
-    {
-        firstName: "Joyal",
-        lastName: "white",
-        email:"joyalwhite@gamil.com",
-        DOB:"21-03-1989",
-    },
-];
 
-// GET request: Retrieve all users
-router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+// GET request: Retrieve all friends
+router.get("/", (req, res) => {
+
+    // Update the code here
+    res.send(JSON.stringify(friends, null, 4));
+
+    res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
-// GET by specific ID request: Retrieve a single user with email ID
-router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+// GET by specific ID request: Retrieve a single friend with email ID
+router.get("/:email", (req, res) => {
+    // Update the code here
+    const email = req.params.email;
+    res.send(friends[email])
 });
 
 
-// POST request: Create a new user
-router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+// POST request: Add a new friend
+router.post("/", (req, res) => {
+    // Update the code here
+    if (req.body.email){
+        friends[req.body.email] = {
+            "firstName":req.body.firstName,
+            "lastName":req.body.lastName,
+            "DOB": req.body.DOB
+            }
+    }
+res.send("The user" + (' ')+ (req.body.firstName) + " Has been added!");
 });
 
 
-// PUT request: Update the details of a user by email ID
+// PUT request: Update the details of a friend with email id
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
-});
+    // Update the code here
+        const email = req.params.email;
+        let friend = friends[email];
+        if (friend) { //Check is friend exists
+            let DOB = req.body.DOB;
+            let firstName = req.body.firstName;
+            let lastName = req.body.lastName;
+    
+            //if DOB the DOB has been changed, update the DOB 
+            if(DOB) {
+                friend["DOB"] = DOB
+            }
+            if(firstName) {
+                friend["firstName"] = firstName;
+            }
+            if(lastName) {
+                friend["lastName"] = lastName;
+            }
+            
+            friends[email]=friend;
+            res.send(`Friend with the email  ${email} updated.`);
+        }
+        else{
+            res.send("Unable to find friend!");
+        }
+      });
 
 
-// DELETE request: Delete a user by email ID
+// DELETE request: Delete a friend by email id
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    // Update the code here
+    const email = req.params.email;
+    if(email){
+        delete friends[email];
+    }
+    res.send(`Friend with the email  ${email} deleted.`);
 });
 
-module.exports=router;
+module.exports = router;
